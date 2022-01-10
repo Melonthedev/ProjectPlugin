b7d3196e9c4ea18bebd1f99c8e1566fb1796133e
Marlon Jestädt <marlon2019@outlook.de>
2022-01-10 21:03:22 +0100

    Update Files

--- src/main/java/wtf/melonthedev/projectplugin/listeners/SignEditListener.java
+++ src/main/java/wtf/melonthedev/projectplugin/listeners/SignEditListener.java
@@ -1,12 +1,10 @@
 package wtf.melonthedev.projectplugin.listeners;
 
-import org.bukkit.ChatColor;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class SignEditListener implements Listener {
