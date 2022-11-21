b292014d5ba9685914b4a7719045b3b0fa0de6b2
Stebadon <stehe@hl-fd.de>
2022-11-21 21:29:13 +0100

    mini-Update

--- src/main/java/wtf/melonthedev/projectplugin/commands/LifestealCommand.java
+++ src/main/java/wtf/melonthedev/projectplugin/commands/LifestealCommand.java
@@ -1,5 +1,6 @@
 package wtf.melonthedev.projectplugin.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -11,6 +12,18 @@ import java.util.List;
 public class LifestealCommand implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        final String prefix = ChatColor.GREEN + ChatColor.BOLD.toString() + "[Life" + ChatColor.DARK_RED + ChatColor.BOLD + "Steal] " + ChatColor.RESET;
+        if (!sender.isOp()){
+            sender.sendMessage(prefix + ChatColor.RED + "You are not allowed to use this command.");
+            return true;
+        }
+        if (args.length == 0){
+            sender.sendMessage(prefix + ChatColor.RED + "Syntax: /lifesteal <setheart | addheart | removeheart | revive>");
+            return true;
+        }
+        if (args.length == 1){
+
+        }
         return false;
     }
 
