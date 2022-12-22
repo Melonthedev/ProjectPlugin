07c3ff156aa9562494aeead75d293e7bcbec9b86
Marlon Jestädt <marlon2019@outlook.de>
2022-12-22 13:02:40 +0100

    Add Tabcomplete

--- src/main/java/wtf/melonthedev/projectplugin/commands/moderation/JoinMessageCommand.java
+++ src/main/java/wtf/melonthedev/projectplugin/commands/moderation/JoinMessageCommand.java
@@ -8,15 +8,20 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import wtf.melonthedev.projectplugin.Main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class JoinMessageCommand implements CommandExecutor {
+public class JoinMessageCommand implements TabExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -69,5 +74,16 @@ public class JoinMessageCommand implements CommandExecutor {
             player.openBook(Main.joinMessages.get(player.getUniqueId()));
             Main.joinMessages.remove(player.getUniqueId());
         }, 10);
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        List<String> tab = new ArrayList<>();
+        if (args.length == 1) {
+            tab.addAll(Bukkit.getOnlinePlayers().stream().map(Player::getName).collect(Collectors.toList()));
+        } else if (args.length == 2 && "reset".startsWith(args[1])) {
+            tab.add("reset");
+        }
+        return tab;
     }
 }
